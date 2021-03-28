d4c13852f2a70f9eed70c74f5524a03b27ad78d1
Christian V <viray.christianj@gmail.com>
2021-03-28 23:16:39 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,6 +7,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location
       t.string :theme
       t.flaot :price
+      t.bolean :
+      t.timestamps
       t.
     end
   end
